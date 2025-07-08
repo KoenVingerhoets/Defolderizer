9fc010d8852dd3a749826073f83f3c171d4f2f93
Koen Vingerhoets <koen.vingerhoets@telenet.be>
2025-07-08 21:53:53 +0200

    Add project files.

---
+++ Defolderizer/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Defolderizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.m_buttonSource = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.m_buttonTarget = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.m_buttonStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // m_buttonSource
+            // 
+            this.m_buttonSource.Location = new System.Drawing.Point(13, 13);
+            this.m_buttonSource.Name = "m_buttonSource";
+            this.m_buttonSource.Size = new System.Drawing.Size(75, 23);
+            this.m_buttonSource.TabIndex = 0;
+            this.m_buttonSource.Text = "Source";
+            this.m_buttonSource.UseVisualStyleBackColor = true;
+            this.m_buttonSource.Click += new System.EventHandler(this.m_buttonSource_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(94, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(344, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(94, 44);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(344, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // m_buttonTarget
+            // 
+            this.m_buttonTarget.Location = new System.Drawing.Point(12, 42);
+            this.m_buttonTarget.Name = "m_buttonTarget";
+            this.m_buttonTarget.Size = new System.Drawing.Size(75, 23);
+            this.m_buttonTarget.TabIndex = 2;
+            this.m_buttonTarget.Text = "Target";
+            this.m_buttonTarget.UseVisualStyleBackColor = true;
+            this.m_buttonTarget.Click += new System.EventHandler(this.m_buttonTarget_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(94, 73);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(344, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // m_buttonStart
+            // 
+            this.m_buttonStart.Location = new System.Drawing.Point(12, 71);
+            this.m_buttonStart.Name = "m_buttonStart";
+            this.m_buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.m_buttonStart.TabIndex = 4;
+            this.m_buttonStart.Text = "Start";
+            this.m_buttonStart.UseVisualStyleBackColor = true;
+            this.m_buttonStart.Click += new System.EventHandler(this.m_buttonStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 26);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter above a list of extensions separated by |\r\nIf there is nothing, all files w" +
+    "ill be copied.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 221);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.m_buttonStart);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.m_buttonTarget);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.m_buttonSource);
+            this.Name = "Form1";
+            this.Text = "Defolderizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button m_buttonSource;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button m_buttonTarget;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button m_buttonStart;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Defolderizer/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Defolderizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void m_buttonSource_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                textBox1.Text = fbd.SelectedPath;            
+        }
+
+        private void m_buttonTarget_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                textBox2.Text = fbd.SelectedPath;
+        }
+
+        private void m_buttonStart_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                m_buttonSource_Click(this, new EventArgs());
+                return;
+            }
+            if (String.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                m_buttonTarget_Click(this, new EventArgs());
+                return;
+            }
+
+            if (String.IsNullOrWhiteSpace(textBox3.Text))
+            {
+                var fileNames = Directory.GetFiles(textBox1.Text, "*.*", SearchOption.AllDirectories);
+                FCopy(fileNames, textBox2.Text);
+            }
+            else
+            {
+                string[] exts = textBox3.Text.Split(new string[]{"|"}, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string s in exts)
+                {
+                    var fileNames = Directory.GetFiles(textBox1.Text, "*." + s.Trim().Replace(".",""), SearchOption.AllDirectories);
+                    FCopy(fileNames, textBox2.Text);
+                }
+            }
+        }
+
+        private void FCopy(String[] fnames, string targetFolder)
+        {
+            foreach (string s in fnames)
+            {
+                if (File.Exists(s))
+                {
+                    String newFile = Path.Combine(targetFolder, Path.GetFileName(s));
+                    if (!File.Exists(newFile))
+                    {
+                        File.Move(s, newFile);
+                    }
+                    else
+                    {
+                        File.Move(s, Path.Combine(targetFolder, Path.GetFileNameWithoutExtension(newFile) + DateTime.Now.Millisecond.ToString()));
+                    }
+                }
+            }
+        }
+    }
+}
